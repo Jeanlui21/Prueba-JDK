5838e8c609127f2404974b690c7a56c5d565bca7
jeanlui21 <jeanlui21@jeanlui21>
2018-09-07 15:45:11 -0500

    Prueba Commit

---
+++ src/main/java/com/tecsup/lab01/Prueba_JDK/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab01.Prueba_JDK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
